75643079d1a2d5a3af43aa62ebb85fb05ac05a7c
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-10-28 16:19:12 -0300

    refactor: Refactoring routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 
 //Rotas
 import { userRoutes } from "./userRoutes";
+import { followRoutes } from "./followRoutes";
 
 //Configurações
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/follow", followRoutes);
 
 export {routes};
