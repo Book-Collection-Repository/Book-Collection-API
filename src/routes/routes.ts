148a4ba138a494fada7519090cd470f15a16450a
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-10-31 19:22:15 -0300

    feat: Creating routes for messages user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,11 +4,13 @@ import { Router } from "express";
 //Rotas
 import { userRoutes } from "./userRoutes";
 import { followRoutes } from "./followRoutes";
+import { messageRoutes } from "./messageRoutes";
 
 //Configurações
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/follow", followRoutes);
+routes.use("/message", messageRoutes);
 
 export {routes};
