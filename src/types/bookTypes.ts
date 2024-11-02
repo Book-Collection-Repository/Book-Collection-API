d9bef845c303fc453f6cbdcecd4bb8645aa6fb14
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-02 11:47:30 -0300

    feat: Creating types for books

---
+++ src/types/bookTypes.ts
@@ -0,0 +1,19 @@
+//Definindo um interface para o tipo de livro
+export interface book {
+    id: string;
+    ISBN_13?: string;
+    ISBN_10?: string;
+    title: string;
+    subTitle?: string;
+    coverImage: string;
+    summary?: string;
+    author: string;
+    publisher: string;
+    publisheData: string;
+    quantityPages: number;
+    mainGenre: string;
+    secondaryGenre?: string;
+};
+
+//Tipo de dados usados nas requisições
+export type CreateBookDTO = Omit<book, 'id'>;
