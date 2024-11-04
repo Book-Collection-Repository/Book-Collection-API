69abefddfe8cbb5ff4da2e842b8c110d3237dd11
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-04 11:04:28 -0300

    buld: Updade schema of the database

--- prisma/migrations/20241102122606_edit_etributtes_in_book/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "BookCollection" DROP CONSTRAINT "BookCollection_bookId_fkey";
-
--- AddForeignKey
-ALTER TABLE "BookCollection" ADD CONSTRAINT "BookCollection_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
