de8f115aecab6b72caa5c913d2292119405f924e
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-04 18:02:32 -0300

    feat: Creating validator for Avaliations

--- src/validators/messageValidator.ts
+++ src/validators/messageValidator.ts
@@ -4,5 +4,5 @@ import { z, ZodError } from 'zod';
 //Validação para criação de uma mensagem
 export const createMessageSchema = z.object({
     content: z.string()
-        .min(3, { message: 'Name must be at least 3 characters long' })
+        .min(3, { message: 'Message must be at least 3 characters long' })
 });
