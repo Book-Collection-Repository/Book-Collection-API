bfa552529b1f9d81688b8480aa74e6833b232d76
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-08 19:27:53 -0300

    refactor: Refactoring book controllers

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -3,24 +3,45 @@ import { Request, Response } from "express";
 
 //Services
 import { BookService } from "../services/BookService";
+import { GoogleGeminiService } from "../services/GoogleGeminiServices";
 
 //Class
 export class BookController {
     private bookService: BookService;
+    private geminiService: GoogleGeminiService;
 
     constructor() {
         this.bookService = new BookService();
-    }
+        this.geminiService = new GoogleGeminiService();
+    };
+
+    //Método para listar livros para pesquisa aleatória
+    async getListBooksRandom(req: Request, res: Response): Promise<Response>{
+        try {
+            
+            const data = await this.bookService.getListRandomBooksForGenres();
+
+            //Retornando lista
+            return res.status(200).json({message: "List of books random", data: data.message});
+
+        } catch (error) {
+            console.error("Error return book: ", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    };
 
     //Método para listar livros do banco de dados por título
     async getBooksInDataBaseForTitle(req: Request, res: Response): Promise<Response> {
         try {
+            //Pegando atributo
             const title = req.params.title;
+            if (!title || title === null || title === undefined) return res.status(400).json({error: "Title not found"});
 
+            //Buscando título no banco de dados
             const getBooks = await this.bookService.searchBooksInDataBaseForTitle(title);
-
             if (!getBooks) return res.status(404).json({ message: "Book not found" });
 
+            //Retornando respostas
             return res.status(200).json({ message: getBooks });
 
         } catch (error) {
@@ -32,10 +53,11 @@ export class BookController {
     //Método para listar livros do banco de dados por ISBN
     async getBooksInDataBaseForISBN(req: Request, res: Response): Promise<Response> {
         try {
-            const isbn = req.params.isbn;
+            const isbn = req.params.isbn; //Pegando ISBN
+            if (!isbn || isbn === null || isbn === undefined) return res.status(400).json({error: "ISBN not found"});
 
+            //Buscando livro no banco de dados
             const getBooks = await this.bookService.searchBookInExternalApiForId(isbn);
-
             if (!getBooks) return res.status(404).json({ message: "Book not found" });
 
             return res.status(200).json({ message: getBooks });
@@ -49,6 +71,7 @@ export class BookController {
     async getBooksInExternalApiForTitle(req: Request, res: Response): Promise<Response> {
         try {
             const title = req.params.title; //Pegando título
+            if (!title || title === null || title === undefined) return res.status(400).json({error: "Title not found"});
 
             //Fazendo e validando a resposta da pesquisa
             const getBooks = await this.bookService.searchBookInExternalApiForTitle(title);
@@ -66,6 +89,7 @@ export class BookController {
     async getBooksInExternalApiForISBN(req: Request, res: Response): Promise<Response> {
         try {
             const isbn = req.params.isbn; //Pegando o identificador
+            if (!isbn || isbn === null || isbn === undefined) return res.status(400).json({error: "ISBN not found"});
 
             //Realizando e validando a busca
             const getBooks = await this.bookService.searchBookInExternalApiForISBN(isbn);
@@ -83,6 +107,7 @@ export class BookController {
     async getBooksInExternalApiForGenre(req: Request, res: Response): Promise<Response> {
         try {
             const genre = req.params.genre; //Busanco por gênero
+            if (!genre || genre === null || genre === undefined) return res.status(400).json({error: "genre not found"});
 
             //Realizando e validando a pesquisa
             const getBooks = await this.bookService.searchBookInExternalApiForGenre(genre);
@@ -100,6 +125,7 @@ export class BookController {
     async getBooksInExternalApiForAuthor(req: Request, res: Response): Promise<Response> {
         try {
             const author = req.params.author; //Buscando autor
+            if (!author || author === null || author === undefined) return res.status(400).json({error: "Author not found"});
 
             //Realizando e validando a pesquisa
             const getBooks = await this.bookService.searchBookInExternalApiForAuthor(author);
