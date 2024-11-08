4663fb97fbaf779ad96db25b197c480770aa9e19
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-11-08 19:29:05 -0300

    refactor: Refactoring book routes

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -6,20 +6,22 @@ import { BookController } from "../controllers/BookController";
 
 //Middleware
 import { authValidationToken } from "../middleware/authValidationToken";
+import { checkingUserExists } from "../middleware/checkingUserExists";
 
 //Configurações
 const bookRoutes = Router();
 const bookController = new BookController();
 
 //Routes
+bookRoutes.get("/list", bookController.getListBooksRandom.bind(bookController));
 bookRoutes.get("/data/title/:title", bookController.getBooksInDataBaseForTitle.bind(bookController));
 bookRoutes.get("/data/isbn/:isbn", bookController.getBooksInDataBaseForISBN.bind(bookController));
 bookRoutes.get("/title/:title", bookController.getBooksInExternalApiForTitle.bind(bookController));
 bookRoutes.get("/isbn/:isbn", bookController.getBooksInExternalApiForISBN.bind(bookController));
 bookRoutes.get("/genre/:genre", bookController.getBooksInExternalApiForGenre.bind(bookController));
 bookRoutes.get("/author/:author", bookController.getBooksInExternalApiForAuthor.bind(bookController));
-bookRoutes.post("/:idBook", authValidationToken, bookController.addtingBookInDataBase.bind(bookController));
-bookRoutes.delete("/:id", authValidationToken, bookController.removeBookInDataBase.bind(bookController));
+bookRoutes.post("/:idBook", authValidationToken, checkingUserExists, bookController.addtingBookInDataBase.bind(bookController));
+bookRoutes.delete("/:id", authValidationToken, checkingUserExists, bookController.removeBookInDataBase.bind(bookController));
 
 //Expontando as rotas
 export { bookRoutes };
